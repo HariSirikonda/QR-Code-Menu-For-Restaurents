af53ce82cb0c0abb3d15506d9d1a095977862e9c
Hari Sirikonda <harisirikonda22@gmail.com>
2025-01-03 11:57:21 +0530

    added generate qr functionality

--- qr_menu/views.py
+++ qr_menu/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .forms import QRCodeForm
+import qrcode
 
 def generate_qr_code(request):
-    form = QRCodeForm()
-    context = { 'form' : form}
-    return render(request, 'generate_qr_code.html', context)
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurent_name']
+            url = form.cleaned_data['URL']
+            
+            #Generate QR Code
+            qr = qrcode.make(url)
+            qr.save('testqr.png')    
+    else:
+        form = QRCodeForm()
+        context = { 'form' : form}
+        return render(request, 'generate_qr_code.html', context)
